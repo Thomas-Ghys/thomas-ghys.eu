fc9d0fbc11007f223fd917f6a355122f9011e161
Thomas Ghys <thomas-ghys@outlook.com>
2024-11-11 01:48:31 +0100

    Implement contact cards
    Signed-off-by: Thomas Ghys <thomas-ghys@outlook.com>

--- libs/contact/contact-feature/src/lib/contact/contact.component.ts
+++ libs/contact/contact-feature/src/lib/contact/contact.component.ts
@@ -1,12 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FeatureContainerComponent} from '@thomas-ghys.eu/common-ui';
+import {
+	EmailContactCardComponent,
+	LinkContactCardComponent,
+	QrContactCardComponent
+} from '@thomas-ghys.eu/contact-ui';
+import {PersonalInfoStore} from '@thomas-ghys.eu/common-data';
+import {BreakpointDirective} from '@thomas-ghys.eu/common-utils';
 
 @Component({
 	selector: 'lib-contact',
 	standalone: true,
-	imports: [CommonModule, FeatureContainerComponent],
+	imports: [
+		CommonModule,
+		FeatureContainerComponent,
+		EmailContactCardComponent,
+		LinkContactCardComponent,
+		QrContactCardComponent,
+		BreakpointDirective
+	],
+	providers: [PersonalInfoStore],
 	templateUrl: './contact.component.html',
 	styleUrl: './contact.component.scss'
 })
-export class ContactComponent {}
+export class ContactComponent {
+	readonly store = inject(PersonalInfoStore);
+
+	openEmail(email: string) {
+		window.location.href = 'mailto:' + email;
+	}
+}
